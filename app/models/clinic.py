3d30bb9b59b15bd288b093080a38f10a196b37f2
AkSQUe <aksque@gmail.com>
2026-03-24 07:59:50 +0200

    Додати модель Clinic для управління клініками

---
+++ app/models/clinic.py
@@ -0,0 +1,18 @@
+from app.extensions import db
+from app.models.mixins import TimestampMixin
+
+
+class Clinic(TimestampMixin, db.Model):
+    __tablename__ = 'clinics'
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(300), nullable=False)
+    slug = db.Column(db.String(200), unique=True, nullable=False)
+    short_description = db.Column(db.String(500))
+    description = db.Column(db.Text)
+    photo = db.Column(db.String(500))
+    sort_order = db.Column(db.Integer, default=0)
+    is_active = db.Column(db.Boolean, default=True, index=True)
+
+    def __repr__(self):
+        return f'<Clinic {self.name}>'
