62e6fe9259f2e434aa688be697b96e6b81b11270
AkSQUe <aksque@gmail.com>
2026-03-25 17:48:24 +0200

    Додати X-Robots-Tag noindex для payments та registration blueprints
    after_request хук на рівні blueprint додає заголовок
    X-Robots-Tag: noindex, nofollow для всіх відповідей платежів
    та реєстрацій.

--- app/payments/__init__.py
+++ app/payments/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 payments_bp = Blueprint('payments', __name__, url_prefix='/payments')
 
+
+@payments_bp.after_request
+def add_noindex_header(response):
+    response.headers['X-Robots-Tag'] = 'noindex, nofollow'
+    return response
+
+
 from app.payments import routes  # noqa: F401,E402
--- app/registration/__init__.py
+++ app/registration/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 registration_bp = Blueprint('registration', __name__, url_prefix='/registration')
 
+
+@registration_bp.after_request
+def add_noindex_header(response):
+    response.headers['X-Robots-Tag'] = 'noindex, nofollow'
+    return response
+
+
 from app.registration import routes  # noqa: F401,E402
